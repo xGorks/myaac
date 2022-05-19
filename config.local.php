6d8db7133b14496e42570aad735f9175cbda23c1
root <root@localhost.localdomain>
2022-05-18 22:03:12 -0400

    config local file

---
+++ config.local.php
@@ -0,0 +1,15 @@
+<?php
+// place for your configuration directives, so you can later easily update myaac
+$config['installed'] = true;
+$config['env'] = 'prod'; // dev or prod
+$config['mail_enabled'] = false;
+$config['server_path'] = '/home/beta/sv/';
+$config['mail_admin'] = 'koliseu-beta@protonmail.com';
+$config['mail_address'] = 'koliseu-beta@protonmail.com';
+$config['date_timezone'] = 'America/Sao_Paulo';
+$config['client'] = '860';
+$config['anonymous_usage_statistics'] = true;
+$config['session_prefix'] = 'myaac_qyvc44ua_';
+$config['cache_prefix'] = 'myaac_t5li8vil_';
+
+$config['highscores_ids_hidden'] = array(2, 3, 4, 5, 6);
